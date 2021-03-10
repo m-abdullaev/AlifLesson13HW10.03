53a03a2e336e733c4d1f5e339a3f0cf0971cbdc7
Maruf Abdullaev <m.abdullaev4188@gmail.com>
2021-03-10 04:15:30 -0500

    Add project files.

---
+++ AlifLesson14HW10.03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlifLesson14HW10._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
